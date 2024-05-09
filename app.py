5849a7fa5dccc7c568f18dd34e6ada5f7e9b27a8
Russell Jarvis <russelljarvis42@gmail.com>
2024-05-09 16:19:57 +1000

    update for web app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# https://jingwen-z.github.io/how-to-draw-a-variety-of-maps-with-folium-in-python/
-import folium
+# https://jingwen-z.github.io,/how-to-draw-a-variety-of-maps-with-folium-in-python/
+#import folium
 #from matplotlib import pyplot as plt
 # importing all necessary libraries
 import geopandas as gpd
@@ -7,15 +7,23 @@ import pandas as pd
 import pdb
 import streamlit as st
 from streamlit_folium import st_folium
+
+#import plotly.express as px
+#import plotly.graph_objects as go
+#from urllib.request import urlopen
+#import json
+##from copy import deepcopy
+#from plotly.subplots import make_subplots
+import folium
 import copy
 from folium.plugins import Draw
+import pickle
 st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 
 
 
 vic_geo = gpd.read_file("suburb-10-vic.geojson")
-vic_inc_map = folium.Map([-36.7569, 144.2786], zoom_start=11,tiles=None)
-folium.TileLayer('CartoDB positron',name="Light Map",control=False).add_to(vic_inc_map)
 
 vic_geo = vic_geo[['vic_loca_2','geometry']]
 Bendigo = vic_geo[vic_geo["vic_loca_2"] == "BENDIGO"]
@@ -31,17 +39,46 @@ labels_enum = [i for (i,J) in enumerate(labels)]
 gdfBendigo = vic_geo[vic_geo["vic_loca_2"].isin(labels)]
 
 
-def main():
+with open("EVCS_Goldfields_geom.p","rb") as f:
+    Bendigodf = pickle.load(f)
 
+#import pdb
+#pdb.set_trace()
+
+
+def main():
+    st.header("EVCS of North Central Catchment Region")
+
+
+    """
+    fig = px.choropleth_mapbox(Bendigodf, geojson=Bendigodf.to_json(), color=Bendigodf.EVC,
+                            color_continuous_scale="Viridis",
+                            range_color=(0, 12),
+                            mapbox_style="carto-positron",
+                            zoom=12, 
+                            center = {"lat": -36.7569, "lon": 144.2786},
+                            opacity=0.1,
+                            labels=Bendigodf.EVC,
+                            width=900,
+                            height=700
+                            )
+
+    """
+    #fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    #fig.show()
+    #st.plotly_chart(fig)
     # Creating a map object
     #melb_inc_map = folium.Map(location=[y_map, x_map], zoom_start=6,tiles=None)
-    gdfBendigo['new_col'] = range(1, len(gdfBendigo) + 1)
+    #gdfBendigo['new_col'] = range(1, len(gdfBendigo) + 1)
     #gdfBendigo
-    # Creating choropleth 
-    folium.Choropleth(geo_data=gdfBendigo,
+    # Creating choropleth
+    vic_inc_map = folium.Map([-36.7569, 144.2786], zoom_start=11,tiles=None)
+    #folium.TileLayer('CartoDB positron',name="Light Map",control=False).add_to(vic_inc_map)
+
+    folium.Choropleth(geo_data=Bendigodf,
                 name='Choropleth',         
-                data=gdfBendigo,
-                columns=['vic_loca_2','geometry'], 
+                data=Bendigodf,
+                columns=['EVC','geometry'],
                 fill_color='YlOrRd',
                 fill_opacity=0.6, 
                 line_opacity=0.8,
@@ -54,28 +91,27 @@ def main():
                                 'color':'#000000', 
                                 'fillOpacity': 0.1, 
                                 'weight': 0.1}
-    SuburbName = folium.features.GeoJson(
-        gdfBendigo,
+    BioRegionName = folium.features.GeoJson(
+        Bendigodf,
         style_function=style_function, 
         control=False,
         tooltip=folium.features.GeoJsonTooltip(
-            fields=['vic_loca_2'],
-            aliases=['SuburbName'],
+            fields=['X_GROUPNAME'],
+            aliases=['EVC'],
             style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
         )
     )
-    vic_inc_map.add_child(SuburbName)
-    vic_inc_map.keep_in_front(SuburbName)
+    vic_inc_map.add_child(BioRegionName)
+    vic_inc_map.keep_in_front(BioRegionName)
     folium.LayerControl().add_to(vic_inc_map)
 
-    folium.LayerControl().add_to(vic_inc_map)
     Draw(export=True).add_to(vic_inc_map)
 
+
     #vic_inc_map.show_in_browser()
-    #st.set_page_config(layout="wide")
     st_data = st_folium(vic_inc_map, width=1725)
 
-    output = st_folium(m, width=700, height=500)
+    #output = st_folium(m, width=700, height=500)
 
 if __name__ == "__main__":
     main()
